1a84b561dc78003ec15b291069ac5407428a8deb
mukuldata <54991370+mukuldata@users.noreply.github.com>
2019-09-08 12:09:48 +0530

    by upload
    area of sphere program

---
+++ Area of Sphere.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float area,radius;
+	float pi=3.142;
+    printf("\n enter the value of radius");
+    scanf("%d%d",&radius,&pi);
+    area=4*pi*radius*radius;
+    printf("\the res is=%f",area);
+    getch();
+}
